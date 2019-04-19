84e248087be7d22a239896aabff2ee6cec076007
Cyril442 <cyril.cieslak@gmail.com>
2019-04-19 15:27:55 +0200

    Initial Commit

---
+++ app/src/main/java/cyril/cieslak/mymynews/Fragments/FragmentThree.kt
@@ -0,0 +1,29 @@
+package cyril.cieslak.mymynews.Fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import cyril.cieslak.mymynews.R
+import kotlinx.android.synthetic.main.fragment_fragment_one.*
+import kotlinx.android.synthetic.main.fragment_fragment_three.*
+
+
+class FragmentThree : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_three, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tv_fragment_three.text = "Le Fragment Three"
+
+    }
+}
