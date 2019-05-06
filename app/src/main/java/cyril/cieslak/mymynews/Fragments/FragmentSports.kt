d9540ff76b17362b7ef43dad0bc49e0472ae9c1a
Cyril442 <cyril.cieslak@gmail.com>
2019-05-06 15:05:03 +0200

    Renamed from Fragment Three to Fragment Sports

--- app/src/main/java/cyril/cieslak/mymynews/Fragments/FragmentSports.kt
+++ app/src/main/java/cyril/cieslak/mymynews/Fragments/FragmentSports.kt
@@ -8,18 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 
 import cyril.cieslak.mymynews.R
-import kotlinx.android.synthetic.main.fragment_fragment_one.*
-import kotlinx.android.synthetic.main.fragment_fragment_three.*
+import kotlinx.android.synthetic.main.fragment_fragment_sports.*
 
 
-class FragmentThree : Fragment() {
+class FragmentSports : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_three, container, false)
+        return inflater.inflate(R.layout.fragment_fragment_sports, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
