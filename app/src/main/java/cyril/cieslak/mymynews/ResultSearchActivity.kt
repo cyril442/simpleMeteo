b020b5ceb33f07c58c7fe81bb17013a8b0a4e767
Cyril442 <cyril.cieslak@gmail.com>
2019-05-09 16:36:07 +0200

    INIT  : Result Search Activity Created. Declared in the Manifest

---
+++ app/src/main/java/cyril/cieslak/mymynews/ResultSearchActivity.kt
@@ -0,0 +1,12 @@
+package cyril.cieslak.mymynews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_search)
+    }
+}
