1f0a2b41bd194c62a440f258835628d11b3ee516
Cyril442 <cyril.cieslak@gmail.com>
2019-05-14 09:31:24 +0200

    The String RequestAPI is going from SearchActivity to ResultSearchActivity. The date is not working properly

--- app/src/main/java/cyril/cieslak/mymynews/ResultSearchActivity.kt
+++ app/src/main/java/cyril/cieslak/mymynews/ResultSearchActivity.kt
@@ -2,6 +2,8 @@ package cyril.cieslak.mymynews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.widget.Toast
 
 class ResultSearchActivity : AppCompatActivity() {
@@ -12,5 +14,12 @@ class ResultSearchActivity : AppCompatActivity() {
 
         val termsForResearchApiFromSearchActivity = intent.getStringExtra(SearchActivity.TERMS_FOR_RESEARCH_API)
         Toast.makeText(this, termsForResearchApiFromSearchActivity, Toast.LENGTH_LONG).show()
+        Log.i("texte", "$termsForResearchApiFromSearchActivity")
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_webview_activity)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
     }
 }
--- app/src/main/java/cyril/cieslak/mymynews/SearchActivity.kt
+++ app/src/main/java/cyril/cieslak/mymynews/SearchActivity.kt
@@ -1,5 +1,7 @@
 package cyril.cieslak.mymynews
 
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,16 +11,19 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import cyril.cieslak.mymynews.Fragments.FragmentTopStories
 import cyril.cieslak.mymynews.Utils.CalendarFragment
 import cyril.cieslak.mymynews.Utils.QueryFragment
 import kotlinx.android.synthetic.main.fragment_calendar.*
 import kotlinx.android.synthetic.main.fragment_check_box.*
 import kotlinx.android.synthetic.main.fragment_search_button.*
+import java.util.Observer
 
 class SearchActivity : AppCompatActivity(), CalendarFragment.CalendarFragmentListener  {
 
     companion object{
         val TERMS_FOR_RESEARCH_API = "TermsForResearchApi"
+
     }
 
 
@@ -30,7 +35,7 @@ class SearchActivity : AppCompatActivity(), CalendarFragment.CalendarFragmentLis
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-
+               // Setting of the Toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar_search_activity)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
@@ -62,42 +67,57 @@ class SearchActivity : AppCompatActivity(), CalendarFragment.CalendarFragmentLis
 
 
             var artsChecked : Boolean = checkBoxArts.isChecked
-            if (artsChecked == true) { checkedArt = "Arts" } else {checkedArt = ""}
+            if (artsChecked == true) { checkedArt = "\"Arts\"" } else {checkedArt = ""}
 
             val politicsChecked : Boolean = checkBoxPolitics.isChecked
-            if (politicsChecked == true) { checkedPolitics = "Politics" } else {checkedPolitics = ""}
+            if (politicsChecked == true) { checkedPolitics = "\"Politics\"" } else {checkedPolitics = ""}
 
             val businessChecked : Boolean = checkBoxBusiness.isChecked
-            if (businessChecked == true) { checkedBusiness = "Business" } else {checkedBusiness = ""}
+            if (businessChecked == true) { checkedBusiness = "\"Business\"" } else {checkedBusiness = ""}
 
             val sportChecked : Boolean = checkBoxSport.isChecked
-            if (sportChecked == true) { checkedSport = "Sport" } else {checkedSport = ""}
+            if (sportChecked == true) { checkedSport = "\"Sports\"" } else {checkedSport = ""}
 
             val entrepreneursChecked : Boolean = checkBoxEntrepreneurs.isChecked
-            if (entrepreneursChecked == true) { checkedEntrepreneurs = "Entrepreneurs" } else {checkedEntrepreneurs = ""}
+            if (entrepreneursChecked == true) { checkedEntrepreneurs = "\"Entrepreneurs\"" } else {checkedEntrepreneurs = ""}
 
             val travelChecked : Boolean = checkBoxTravel.isChecked
-            if (travelChecked == true) { checkedTravel = "Travel" } else {checkedTravel = ""}
+            if (travelChecked == true) { checkedTravel = "\"Travel\"" } else {checkedTravel = ""}
 
 
-//            var begin = calendarFragment.entryDate
-//            var end = calendarFragment.endDate
+
+
+
+            var begin = calendarFragment.entryDate
+            var end = calendarFragment.endDate
+
+
 
 
             Toast.makeText(this, "Search Button Clicked", Toast.LENGTH_SHORT).show()
-            Log.i("Texte affiché ", " $querytext, $checkedArt, $checkedPolitics, $checkedBusiness, $checkedSport, $checkedEntrepreneurs, $checkedTravel")
+ //           Log.i("Texte affiché ", "$querytext, $checkedArt, $checkedPolitics, $checkedBusiness, $checkedSport, $checkedEntrepreneurs, $checkedTravel, $begin, $end ")
 
             //     Log.i("BEG", " $begin ")
+            var stringWithTermsForRequest = ("$checkedArt$checkedPolitics$checkedBusiness$checkedSport$checkedEntrepreneurs$checkedTravel").trim()
+            var termsForResearchApi =  "$stringWithTermsForRequest".replace("\\s".toRegex(), "")
 
-            var termsForResearchApi = " $querytext, $checkedArt, $checkedPolitics, $checkedBusiness, $checkedSport, $checkedEntrepreneurs, $checkedTravel"
 
-           var intent = Intent(button.context, ResultSearchActivity::class.java)
-            // To add some putExtra with all the strategic words to
-            intent.putExtra(TERMS_FOR_RESEARCH_API, "$termsForResearchApi")
+
+
+            val stringForRequest = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=$begin&end_date=$end&q=$querytext&fq=news_desk($termsForResearchApi)&sort=relevance&api-key=92Nbf4KeZSKhJXGm5QA3eTgNJjFW61gW"
+            Log.i("Textes", "$termsForResearchApi")
+
+            var intent = Intent(button.context, ResultSearchActivity::class.java)
+            // The string for the APi request to the server:
+            intent.putExtra(TERMS_FOR_RESEARCH_API, "$stringForRequest")
+
             button_search.context.startActivity(intent)
 
 
         })
 
     }
+
+
+
 }
