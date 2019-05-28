4a8c097f51ccde259886203d51243d1fd2cf43f8
Cyril442 <cyril.cieslak@gmail.com>
2019-05-28 13:00:26 +0200

    working but only with Log, not with a function

--- app/src/main/java/cyril/cieslak/mymynews/NotificationWorker.kt
+++ app/src/main/java/cyril/cieslak/mymynews/NotificationWorker.kt
@@ -3,20 +3,41 @@ package cyril.cieslak.mymynews
 import android.util.Log
 import androidx.work.Worker
 import cyril.cieslak.mymynews.NotificationActivity
-
+import kotlinx.android.synthetic.main.fragment_notification_enable.*
 
 
 class NotificationWorker : Worker() {
 
-    val notificationActivity = NotificationActivity()
+
 
     override fun doWork(): Result {
 
-        Log.i("testWorker", "Starting hard work")
-        Thread.sleep(2000)
-        notificationActivity.newsNotificationOn()
-        Log.i("testWorker", "work finished")
+        try {
 
+            Log.i("testWorker", "Starting Sleep work")
+            Thread.sleep(2000)
+            Log.i("testWorker", "Sleep work finished")
+
+        } catch (e: Exception) {
+        return Result.FAILURE
+    }
+
+        try {
+
+            Log.i("testWorker", "Starting NewsNotification")
+            //   notificationActivity.newsNotificationOn()
+          // retrievedNewsNotificationsOn()
+
+            Log.i("testWorker", "NewsNotification Finished")
+
+        } catch (e: Exception) {
+            return Result.FAILURE
+
+
+        }
         return Result.SUCCESS
     }
+
+
+
 }
