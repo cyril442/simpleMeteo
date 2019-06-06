d161472384db901adfaa7031b1a36e000a15bdfa
Cyril442 <cyril.cieslak@gmail.com>
2019-06-06 14:17:28 +0200

    For testing, added a function included into the Class. Not able to make work with an external function from the Notification Activity

--- app/src/main/java/cyril/cieslak/mymynews/NotificationWorker.kt
+++ app/src/main/java/cyril/cieslak/mymynews/NotificationWorker.kt
@@ -1,21 +1,26 @@
 package cyril.cieslak.mymynews
 
+import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import cyril.cieslak.mymynews.NotificationActivity
+import cyril.cieslak.mymynews.Parsers.parseDatas
 import kotlinx.android.synthetic.main.fragment_notification_enable.*
 
 
-class NotificationWorker : Worker() {
 
+class NotificationWorker  : Worker() {
 
 
     override fun doWork(): Result {
+
+
 
         try {
 
             Log.i("testWorker", "Starting Sleep work")
             Thread.sleep(2000)
+            var threadPassed = "ThreadPassed"
             Log.i("testWorker", "Sleep work finished")
 
         } catch (e: Exception) {
@@ -27,7 +32,8 @@ class NotificationWorker : Worker() {
             Log.i("testWorker", "Starting NewsNotification")
             //   notificationActivity.newsNotificationOn()
           // retrievedNewsNotificationsOn()
-
+            var expensive = doExpensiveCalculation()
+            Log.i("testWorker", "expensive : $expensive")
             Log.i("testWorker", "NewsNotification Finished")
 
         } catch (e: Exception) {
@@ -37,6 +43,13 @@ class NotificationWorker : Worker() {
         }
         return Result.SUCCESS
     }
+
+
+
+fun doExpensiveCalculation() : String {
+    var doexpensive = "123"
+    return doexpensive
+}
 
 
 
