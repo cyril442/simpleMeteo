3f63f2ef992fa26c098408984926798d6798406e
Cyril442 <cyril.cieslak@gmail.com>
2019-06-06 14:29:29 +0200

    External class created to clarifiy the SearchActivity and Notification

--- app/src/main/java/cyril/cieslak/mymynews/UtilsClass/checkBoxEmptyOrNot.kt
+++ app/src/main/java/cyril/cieslak/mymynews/UtilsClass/checkBoxEmptyOrNot.kt
@@ -1,134 +1,131 @@
 package cyril.cieslak.mymynews.UtilsClass
 
 import android.graphics.Color
+import android.support.v4.content.res.ResourcesCompat.getColor
 import android.util.Size
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import cyril.cieslak.mymynews.R
 import kotlinx.android.synthetic.main.fragment_check_box.*
 import kotlinx.android.synthetic.main.fragment_search_button.*
+import kotlin.properties.Delegates
 
 
-
-class checkBoxEmptyOrNot () {
-
+class checkBoxEmptyOrNot() {
 
 
     ///// --- CHECK BOX Tested Empty or Not --- ///////
 
-    var artsIsCheckedOrNot = false
-    var businessIsCheckedOrNot = false
-    var entrepreneursIsCheckedOrNot = false
-    var politicsIsCheckedOrNot = false
-    var sportsIsCheckedOrNot = false
-    var travelIsCheckedOrNot = false
+
+    fun checkBoxStatus(
+        button_search: Button,
+        checkBoxArts: CheckBox,
+        checkBoxBusiness: CheckBox,
+        checkBoxEntrepreneurs: CheckBox,
+        checkBoxPolitics: CheckBox,
+        checkBoxSport: CheckBox,
+        checkBoxTravel: CheckBox
+    ) {
 
 
 
+        checkBoxArts.setOnClickListener {
 
-fun checkBoxStatus (editTextSize: Int, button_search: Button, checkBoxArts: CheckBox, checkBoxBusiness : CheckBox, checkBoxEntrepreneurs : CheckBox, checkBoxPolitics : CheckBox, checkBoxSport: CheckBox, checkBoxTravel : CheckBox) {
+          //  var textSize = textSizeObservableToInt
 
-    checkBoxArts.setOnClickListener {
-        if (!checkBoxArts.isChecked) {
-            artsIsCheckedOrNot = false
-        } else {
-            artsIsCheckedOrNot = true
+            when (checkBoxArts.isChecked) {
+                true -> {
+                    button_search.isEnabled = true
+                    button_search.setBackgroundResource(R.color.bluelight)
+                }
+                false -> {
+                    if (!checkBoxBusiness.isChecked && !checkBoxEntrepreneurs.isChecked && !checkBoxPolitics.isChecked && !checkBoxSport.isChecked && !checkBoxTravel.isChecked) {
+                        button_search.isEnabled = false
+                        button_search.setBackgroundResource(R.color.colorPrimary)
+                    }
+                }
+
+            }
         }
- //       Toast.makeText(, "$artsIsCheckedOrNot", Toast.LENGTH_SHORT).show()
-        if (editTextSize >= 1 && artsIsCheckedOrNot) {
-   //         Toast.makeText(this, "Ready to Make the Button Enable because Arts is Checked", Toast.LENGTH_SHORT)
-   //             .show()
-            button_search.isEnabled = true
-            button_search.setBackgroundColor(Color.BLUE)
+
+        checkBoxBusiness.setOnClickListener {
+         //   var textSize = textSizeObservableToInt
+            when (checkBoxBusiness.isChecked) {
+                true -> {
+                    button_search.isEnabled = true
+                    button_search.setBackgroundResource(R.color.bluelight)
+                }
+                false -> {
+                    if (!checkBoxArts.isChecked && !checkBoxEntrepreneurs.isChecked && !checkBoxPolitics.isChecked && !checkBoxSport.isChecked && !checkBoxTravel.isChecked) {
+                        button_search.isEnabled = false
+                        button_search.setBackgroundResource(R.color.colorPrimary)
+                    }
+                }
+            }
+        }
+
+        checkBoxEntrepreneurs.setOnClickListener {
+          //  var textSize = textSizeObservableToInt
+            when (checkBoxEntrepreneurs.isChecked) {
+                true -> {
+                    button_search.isEnabled = true
+                    button_search.setBackgroundResource(R.color.bluelight)
+                }
+                false -> {
+                    if (!checkBoxArts.isChecked && !checkBoxBusiness.isChecked && !checkBoxPolitics.isChecked && !checkBoxSport.isChecked && !checkBoxTravel.isChecked) {
+                        button_search.isEnabled = false
+                        button_search.setBackgroundResource(R.color.colorPrimary)
+                    }
+                }
+            }
+        }
+
+        checkBoxPolitics.setOnClickListener {
+         //   var textSize = textSizeObservableToInt
+            when (checkBoxPolitics.isChecked) {
+                true -> {
+                    button_search.isEnabled = true
+                    button_search.setBackgroundResource(R.color.bluelight)
+                }
+                false -> {
+                    if (!checkBoxArts.isChecked && !checkBoxBusiness.isChecked && !checkBoxEntrepreneurs.isChecked && !checkBoxSport.isChecked && !checkBoxTravel.isChecked) {
+                        button_search.isEnabled = false
+                        button_search.setBackgroundResource(R.color.colorPrimary)
+                    }
+                }
+            }
+        }
+
+        checkBoxSport.setOnClickListener {
+         //   var textSize = textSizeObservableToInt
+            when (checkBoxSport.isChecked) {
+                true -> {
+                    button_search.isEnabled = true
+                    button_search.setBackgroundResource(R.color.bluelight)
+                }
+                false -> {
+                    if (!checkBoxArts.isChecked && !checkBoxBusiness.isChecked && !checkBoxEntrepreneurs.isChecked && !checkBoxPolitics.isChecked && !checkBoxTravel.isChecked) {
+                        button_search.isEnabled = false
+                        button_search.setBackgroundResource(R.color.colorPrimary)
+                    }
+                }
+            }
+        }
+
+        checkBoxTravel.setOnClickListener {
+       //     var textSize = textSizeObservableToInt
+            when (checkBoxTravel.isChecked) {
+                true -> {
+                    button_search.isEnabled = true
+                    button_search.setBackgroundResource(R.color.bluelight)
+                }
+                false -> {
+                    if (!checkBoxArts.isChecked && !checkBoxBusiness.isChecked && !checkBoxEntrepreneurs.isChecked && !checkBoxPolitics.isChecked && !checkBoxSport.isChecked) {
+                        button_search.isEnabled = false
+                        button_search.setBackgroundResource(R.color.colorPrimary)
+                    }
+                }
+            }
         }
     }
-
-    checkBoxBusiness.setOnClickListener{
-        if (!checkBoxBusiness.isChecked) {
-            businessIsCheckedOrNot = false
-        } else {
-            businessIsCheckedOrNot = true
-        }
-   //     Toast.makeText(this, "$businessIsCheckedOrNot", Toast.LENGTH_SHORT).show()
-        if (editTextSize >= 1 && businessIsCheckedOrNot == true) {
- //           Toast.makeText(this, "Ready to Make the Button Enable because Business is Checked", Toast.LENGTH_SHORT)
-   //             .show()
-            button_search.isEnabled = true
-            button_search.setBackgroundColor(Color.BLUE)
-
-        }
-    }
-
-    checkBoxEntrepreneurs.setOnClickListener{
-        if (!checkBoxEntrepreneurs.isChecked) {
-            entrepreneursIsCheckedOrNot = false
-        } else {
-            entrepreneursIsCheckedOrNot = true
-        }
-   //     Toast.makeText(this, "$entrepreneursIsCheckedOrNot", Toast.LENGTH_SHORT).show()
-        if (editTextSize >= 1 && entrepreneursIsCheckedOrNot == true) {
-  //          Toast.makeText(
-//                this,
-//                "Ready to Make the Button Enable because Entrepreneurs is Checked",
-//                Toast.LENGTH_SHORT
-//            ).show()
-            button_search.isEnabled = true
-            button_search.setBackgroundColor(Color.BLUE)
-
-        }
-    }
-
-    checkBoxPolitics.setOnClickListener{
-        if (!checkBoxPolitics.isChecked) {
-            politicsIsCheckedOrNot = false
-        } else {
-            politicsIsCheckedOrNot = true
-        }
-     //   Toast.makeText(this, "$politicsIsCheckedOrNot", Toast.LENGTH_SHORT).show()
-        if (editTextSize >= 1 && politicsIsCheckedOrNot == true) {
-//            Toast.makeText(this, "Ready to Make the Button Enable because politics is Checked", Toast.LENGTH_SHORT)
-//                .show()
-            button_search.isEnabled = true
-            button_search.setBackgroundColor(Color.BLUE)
-
-        }
-    }
-
-    checkBoxSport.setOnClickListener{
-        if (!checkBoxSport.isChecked) {
-            sportsIsCheckedOrNot = false
-        } else {
-            sportsIsCheckedOrNot = true
-        }
-     //   Toast.makeText(this, "$sportsIsCheckedOrNot", Toast.LENGTH_SHORT).show()
-        if (editTextSize >= 1 && sportsIsCheckedOrNot == true) {
-//            Toast.makeText(this, "Ready to Make the Button Enable because Sport is Checked", Toast.LENGTH_SHORT)
-//                .show()
-            button_search.isEnabled = true
-            button_search.setBackgroundColor(Color.BLUE)
-
-        }
-    }
-
-    checkBoxTravel.setOnClickListener{
-        if (!checkBoxTravel.isChecked) {
-            travelIsCheckedOrNot = false
-        } else {
-            travelIsCheckedOrNot = true
-        }
-   //     Toast.makeText(this, "$travelIsCheckedOrNot", Toast.LENGTH_SHORT).show()
-        if (editTextSize >= 1 && travelIsCheckedOrNot == true) {
-//            Toast.makeText(this, "Ready to Make the Button Enable because Travel is Checked", Toast.LENGTH_SHORT)
-//                .show()
-            button_search.isEnabled = true
-            button_search.setBackgroundColor(Color.BLUE)
-
-        }
-    }
-
 }
-//        var theCheckBoxStringTrueFalse =
-//            "$artsIsCheckedOrNot $businessIsCheckedOrNot $entrepreneursIsCheckedOrNot $politicsIsCheckedOrNot $sportsIsCheckedOrNot $travelIsCheckedOrNot".trim()
-//        var validCheckBoxString = theCheckBoxStringTrueFalse.contains("true", ignoreCase = false)
-}
+
